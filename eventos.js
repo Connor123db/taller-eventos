a69469baf9763189c42c84bec4f732ce8ef82cba
Connor123db <igoroock12345@gmail.com>
2025-08-13 15:20:15 -0300

    Solución: evitar propagación del clic en el botón

---
+++ eventos.js
@@ -0,0 +1,8 @@
+// eventos.js
+// Selecciona el div contenedor
+const contenedor = document.getElementById("contenedor");
+
+// Agrega el manejador de eventos
+contenedor.addEventListener("click", () => {
+  alert("Hola! Soy el div");
+});
